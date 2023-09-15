478631fbd4a689f3e1fa5d8506a3664e4fe70afd
Juan Clemente <j.clemente3@alumnos.santotomas.cl>
2023-09-15 20:56:14 -0300

    Subida 2

--- app/src/main/java/com/example/evaluacion1/input_screen.java
+++ app/src/main/java/com/example/evaluacion1/input_screen.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 
 public class input_screen extends AppCompatActivity {
     //Creamos las variables que trabajarán con los id de esta pantalla
@@ -16,14 +17,15 @@ public class input_screen extends AppCompatActivity {
     Button bttnCalcular;
     Button bttnLimpiar;
 
+    double resultado;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_screen);
 
-        //tomamos los campos number y los asignamos a costo y hectarea
-        costo = findViewById(R.id.input_costoVerduras);
-        hectarea = findViewById(R.id.input_totalHectarea);
+        costo = findViewById(R.id.input_costo);
+        hectarea = findViewById(R.id.input_hectarea);
 
         //asignamos los botones
         bttnCalcular = findViewById(R.id.bttnCalcular);
@@ -33,10 +35,16 @@ public class input_screen extends AppCompatActivity {
         bttnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                //obtenemos la cadena de texto para luego hacerle un parseDouble.
+                String nums1 = costo.getText().toString();
+                String nums2 = hectarea.getText().toString();
+                double num1 = Double.parseDouble(nums1);
+                double num2 = Double.parseDouble(nums2);
+                double resultado = num1 * num2;
                 Intent intent = new Intent(
                         getApplicationContext(), output_screen.class
                 );
-                intent.putExtra("mensaje", texto.getText().toString());
+                intent.putExtra("resultado", texto.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/evaluacion1/output_screen.java
+++ app/src/main/java/com/example/evaluacion1/output_screen.java
@@ -3,12 +3,18 @@ package com.example.evaluacion1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class output_screen extends AppCompatActivity {
 
+    TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_output_screen);
+
+        result = findViewById(R.id.txtResultado);
+        String resultado = getIntent().getStringExtra("resultado");
+        result.setText(resultado);
     }
 }
